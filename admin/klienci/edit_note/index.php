95476a6d6efa8513ea88e1a836cc13ebabeaf345
Mikołaj Chalimoniuk <jettiimc.pl@gmail.com>
2024-12-09 13:58:28 +0100

    d2

--- admin/klienci/edit_note/index.php
+++ admin/klienci/edit_note/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['admin']) || (isset($_SESSION['admin'])&&$_SESSION['admin']==0)){
+if((!isset($_SESSION['admin']) || (isset($_SESSION['admin'])&&$_SESSION['admin']==0)) && (!isset($_SESSION['employee']) || (isset($_SESSION['employee'])&&$_SESSION['employee']==0))){
     header("location: https://chalimoniukmikolaj.infinityfreeapp.com/");
 }
 require_once("../../../scripts/database.php");
